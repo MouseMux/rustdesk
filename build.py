629a88652c08949cfe4a9ec6c0ce5f9288b5c6f2
MouseMux <info@mousemux.com>
2025-10-17 20:38:02 +0200

    Fix python3 command for Windows compatibility
    - Change python3 to platform-dependent command (python on Windows, python3 on Linux/Mac)
    - Fixes portable installer generation step on Windows
    - Uses existing 'windows' variable for platform detection

--- build.py
+++ build.py
@@ -446,8 +446,9 @@ def build_flutter_windows(version, features, skip_portable_pack):
         return
     os.chdir('libs/portable')
     system2('pip3 install -r requirements.txt')
+    python_cmd = 'python' if windows else 'python3'
     system2(
-        f'python3 ./generate.py -f ../../{flutter_build_dir_2} -o . -e ../../{flutter_build_dir_2}/rustdesk.exe')
+        f'{python_cmd} ./generate.py -f ../../{flutter_build_dir_2} -o . -e ../../{flutter_build_dir_2}/rustdesk.exe')
     os.chdir('../..')
     if os.path.exists('./rustdesk_portable.exe'):
         os.replace('./target/release/rustdesk-portable-packer.exe',
@@ -509,9 +510,10 @@ def main():
         system2(
             f'cp -rf target/release/RustDesk.exe {res_dir}')
         os.chdir('libs/portable')
+        python_cmd = 'python' if windows else 'python3'
         system2('pip3 install -r requirements.txt')
         system2(
-            f'python3 ./generate.py -f ../../{res_dir} -o . -e ../../{res_dir}/rustdesk-{version}-win7-install.exe')
+            f'{python_cmd} ./generate.py -f ../../{res_dir} -o . -e ../../{res_dir}/rustdesk-{version}-win7-install.exe')
         system2('mv ../../{res_dir}/rustdesk-{version}-win7-install.exe ../..')
     elif os.path.isfile('/usr/bin/pacman'):
         # pacman -S -needed base-devel
